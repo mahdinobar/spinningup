ced5aa4cbfe94d79abed193a8c38d06251882b5b
mahdinobar <nobar.mahdi@gmail.com>
2024-03-13 20:09:20 +0100

    clean start

--- spinup/examples/pytorch/inverted_pendulum_SAC.py
+++ spinup/examples/pytorch/inverted_pendulum_SAC.py
@@ -11,21 +11,21 @@ import os
 
 TRAIN=False
 DEBUG=True
+env_fn = lambda: gym.make('Pendulum-v0')
 if __name__ == '__main__':
     if TRAIN:
         # train
-        env_fn = lambda: gym.make('Pendulum-v0')
         exp_name="logs"
         output_dir='/home/mahdi/ETHZ/codes/spinningup/spinup/examples/pytorch/'+exp_name
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
         logger_kwargs = dict(output_dir=output_dir, exp_name=exp_name)
-        sac(env_fn, ac_kwargs={}, seed=0, steps_per_epoch=1000, epochs=50, replay_size=100000, gamma=0.99, polyak=0.995, lr=0.001, alpha=0.2, batch_size=100, start_steps=1000, update_after=200, update_every=50, num_test_episodes=10, max_ep_len=1000, logger_kwargs=logger_kwargs, save_freq=1)
+        sac(env_fn, ac_kwargs={}, seed=0, steps_per_epoch=1000, epochs=2, replay_size=100000, gamma=0.99, polyak=0.995, lr=0.001, alpha=0.2, batch_size=100, start_steps=1000, update_after=200, update_every=50, num_test_episodes=10, max_ep_len=1000, logger_kwargs=logger_kwargs, save_freq=1)
     if DEBUG:
-        from gym.wrappers.monitoring import VideoRecorder
-        VideoRecorder(env_fn,'/home/mahdi/ETHZ/codes/spinningup/spinup/examples/pytorch/logs')
+        env_fn = gym.make('Pendulum-v0')
+        # from gym.wrappers.monitoring.video_recorder import VideoRecorder
+        # VideoRecorder(env_fn,'/home/mahdi/ETHZ/codes/spinningup/spinup/examples/pytorch/logs/video.mp4', enabled=True)
         # visualize output
         _, get_action = load_policy_and_env('/home/mahdi/ETHZ/codes/spinningup/spinup/examples/pytorch/logs')
-        env_fn = gym.make('Pendulum-v0')
         run_policy(env_fn, get_action,num_episodes=3)
 
