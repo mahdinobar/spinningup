e4381e10ff3e2755e4c29b9b9fe16e2f182116a8
mahdinobar <nobar.mahdi@gmail.com>
2024-04-09 19:14:28 +0200

    Fepv0_0 stable added

--- spinup/examples/pytorch/Hybrid_SAC.py
+++ spinup/examples/pytorch/Hybrid_SAC.py
@@ -9,8 +9,8 @@ from spinup.utils.test_policy import load_policy_and_env, run_policy
 import os
 
 TRAIN=False
-env_fn = lambda: gym.make('Tworr-v0')
-exp_name = "Tworrv0_28" #"Tworrv0_27" #"no_SAC_0_9"
+env_fn = lambda: gym.make('Fep-v0')
+exp_name = "Fepv0_0" #"Tworrv0_27" #"no_SAC_0_9"
 if __name__ == '__main__':
     if TRAIN:
         # train
@@ -18,7 +18,7 @@ if __name__ == '__main__':
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
         logger_kwargs = dict(output_dir=output_dir, exp_name=exp_name)
-        sac(env_fn, ac_kwargs={}, seed=0, steps_per_epoch=100, epochs=300, replay_size=100000, gamma=0.99, polyak=0.995,
+        sac(env_fn, ac_kwargs={}, seed=0, steps_per_epoch=100, epochs=3, replay_size=100000, gamma=0.99, polyak=0.995,
             lr=0.005, alpha=0.2, batch_size=100, start_steps=99, update_after=99, update_every=100, num_test_episodes=5,
             max_ep_len=100, logger_kwargs=logger_kwargs, save_freq=1, initial_actions="zero")
     else:
@@ -27,5 +27,6 @@ if __name__ == '__main__':
         # VideoRecorder(env_fn,'/home/mahdi/ETHZ/codes/spinningup/spinup/examples/pytorch/logs/video.mp4', enabled=True)
         # visualize output
         output_dir='/home/mahdi/ETHZ/codes/spinningup/spinup/examples/pytorch/logs/'+exp_name
-        env, get_action = load_policy_and_env(output_dir)
+        env_loaded, get_action = load_policy_and_env(output_dir)
+        env=env_fn()
         run_policy(env, get_action,num_episodes=1, output_dir=output_dir)
--- spinup/utils/logx.py
+++ spinup/utils/logx.py
@@ -366,7 +366,7 @@ class EpochLogger(Logger):
             v = self.epoch_dict[key]
             vals = np.concatenate(v) if isinstance(v[0], np.ndarray) and len(v[0].shape)>0 else v
             stats = mpi_statistics_scalar(vals, with_min_and_max=with_min_and_max)
-            super().log_tabular(key if average_only else 'Average' + key, stats[0])
+            super().log_tabular(key if average_only else 'Average' + key, stats[0]) #AverageEpRet header is here
             if not(average_only):
                 super().log_tabular('Std'+key, stats[1])
             if with_min_and_max:
