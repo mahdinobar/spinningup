3c478e330c86b453a2c52347903e27a46e4c09a2
mahdinobar <nobar.mahdi@gmail.com>
2024-04-21 14:55:39 +0200

    no_SAC_v0_8

--- spinup/examples/pytorch/Hybrid_SAC.py
+++ spinup/examples/pytorch/Hybrid_SAC.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
         logger_kwargs = dict(output_dir=output_dir, exp_name=exp_name)
         sac(env_fn, ac_kwargs={}, seed=0, steps_per_epoch=100, epochs=5, replay_size=1000000, gamma=0.99, polyak=0.995,
             lr=0.005, alpha=0.2, batch_size=100, start_steps=10000, update_after=100, update_every=100, num_test_episodes=2,
-            max_ep_len=100, logger_kwargs=logger_kwargs, save_freq=1, initial_actions="zero", save_buffer=True, sample_mode = 1)
+            max_ep_len=np.inf, logger_kwargs=logger_kwargs, save_freq=1, initial_actions="zero", save_buffer=True, sample_mode = 1)
     else:
         output_dir='/home/mahdi/ETHZ/codes/spinningup/spinup/examples/pytorch/logs/'+exp_name
         env_loaded, get_action = load_policy_and_env(output_dir)
