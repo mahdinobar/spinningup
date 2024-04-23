31d83b668d31452eb4b6e971ec15ffddc8667bc5
mahdinobar <nobar.mahdi@gmail.com>
2024-04-23 15:54:39 +0200

    Fepv0_8_10

--- spinup/examples/pytorch/Hybrid_SAC.py
+++ spinup/examples/pytorch/Hybrid_SAC.py
@@ -7,9 +7,9 @@ import spinup.algos.pytorch.sac.core as core
 from spinup.utils.test_policy import load_policy_and_env, run_policy
 import os
 
-TRAIN=1
+TRAIN=0
 env_fn = lambda: gym.make('Fep-v0')
-exp_name = "Fepv0_8_10_pstarAddedState"
+exp_name = "Fepv0_8_10"
 if __name__ == '__main__':
     if TRAIN:
         # train
