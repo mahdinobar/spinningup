009f9e83fca2e868eb8f1a8744568a09328e2060
mahdinobar <nobar.mahdi@gmail.com>
2024-05-01 13:09:33 +0200

    Fepv0_10_1

--- spinup/backup_gym/gym/envs/classic_control/fep.py
+++ spinup/backup_gym/gym/envs/classic_control/fep.py
@@ -91,9 +91,9 @@ Robotic Manipulation" by Murry et al.
         self.observation_space = spaces.Box(low=low_s, high=high_s, dtype=np.float32)
         # Attention just 6 DOF is simulated (7th DOF is disabled)
         # Attention: limits of SAC actions
-        high_a = 0.05 * np.array([2.1750, 2.1750, 2.1750, 2.1750, 2.6100,
-                                  2.6100])  # TODO Attention: limits should be the same otherwise modify sac code
-        # high_a = 0.05 * np.array([2.1750, 2.1750, 2.1750])  # TODO Attention: limits should be the same otherwise modify sac code
+        # high_a = 0.05 * np.array([2.1750, 2.1750, 2.1750, 2.1750, 2.6100,
+        #                           2.6100])  # TODO Attention: limits should be the same otherwise modify sac code
+        high_a = np.array([2, 0.5, 0.1])
         low_a = -high_a
         self.action_space = spaces.Box(low=low_a, high=high_a, dtype=np.float32)
         # output_dir_rendering = "/home/mahdi/ETHZ/codes/spinningup/spinup/examples/pytorch/logs/"
@@ -114,7 +114,7 @@ Robotic Manipulation" by Murry et al.
             pinvA = np.linalg.lstsq((np.matmul(A, A.T) + ld * ld * np.eye(m, m)).T, A, rcond=None)[0].T
         return pinvA
 
-    def q_command(self, r_ee, v_ee, Jpinv, rd, vd, e, dt):
+    def q_command(self, r_ee, v_ee, Jpinv, rd, vd, e, dt, a):
         """
         PID Traj Tracking Feedback Controller
         Inputs:
@@ -126,7 +126,7 @@ Robotic Manipulation" by Murry et al.
         """
         e_t = (rd - r_ee)
         e = np.vstack((e, e_t.reshape(1, 3)))
-        v_command = vd + self.K_p * e_t + self.K_i * np.sum(e[1:, :], 0) * dt + self.K_d * (vd - v_ee)
+        v_command = vd + (self.K_p+a[0]) * e_t + (self.K_i+a[1]) * np.sum(e[1:, :], 0) * dt + (self.K_d+a[2]) * (vd - v_ee)
         dqc = np.dot(Jpinv, v_command)
         return dqc, e
 
@@ -264,9 +264,9 @@ Robotic Manipulation" by Murry et al.
         J_t = np.asarray(linearJacobian)[:, :6]
         Jpinv_t = self.pseudoInverseMat(J_t, ld=0.1)  # TODO: check pseudo-inverse damping coefficient
         dqc_t, self.e = self.q_command(r_ee=r_hat_t, v_ee=v_hat_t, Jpinv=Jpinv_t, rd=rd_t, vd=vd_t, e=self.e,
-                                       dt=dt)
+                                       dt=dt, a=a)
         # inject SAC action
-        dqc_t = dqc_t + a
+        dqc_t = dqc_t
         # TODO check
         # command joint speeds (only 6 joints)
         pb.setJointMotorControlArray(
@@ -460,7 +460,7 @@ Robotic Manipulation" by Murry et al.
                 pb.stepSimulation(physicsClientId=physics_client)
                 time.sleep(0.01)
 
-        render_test_buffer = True
+        render_test_buffer = False
         if render_test_buffer == True:
             fig1, axs1 = plt.subplots(3, 1, sharex=False, sharey=False, figsize=(7, 14))
             axs1[0].plot(self.plot_data_buffer[:, 3], self.plot_data_buffer[:, 4], 'r--', label='EE desired traj')
@@ -584,9 +584,9 @@ Robotic Manipulation" by Murry et al.
             buf_obs2 = np.load(output_dir_rendering + "/buf_obs2.npy")
             idx_last=np.where(np.sum(buf_obs, 1) == 0)[0][0]
             fig6, axs6 = plt.subplots(3, 1, sharex=False, sharey=False, figsize=(16, 10))
-            idx_start=9000
-            idx_end=300*100 #1210
-            idx_vline=idx_start+100-1
+            idx_start=0
+            idx_end=110*100 #1210
+            idx_vline=idx_start+10000-1
             x=range(idx_start, idx_end, 1)
             y=buf_obs[0:idx_last, :][idx_start:idx_end, 0]
             axs6[0].plot(x, y, 'b', linewidth=0.08, marker=".", markersize=2, label='r_hat_tp1[0] - rd_t[0]')
