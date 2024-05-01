9ab453225d71bae12952cd7d571dcd8d7a6a1a9d
mahdinobar <nobar.mahdi@gmail.com>
2024-05-01 17:42:21 +0200

    Fepv0_12_6

--- spinup/examples/pytorch/Hybrid_SAC.py
+++ spinup/examples/pytorch/Hybrid_SAC.py
@@ -9,7 +9,7 @@ import os
 
 TRAIN=1
 env_fn = lambda: gym.make('Fep-v0')
-exp_name = "Fepv0_12_5"
+exp_name = "Fepv0_12_6"
 if __name__ == '__main__':
     if TRAIN:
         # train
@@ -18,7 +18,7 @@ if __name__ == '__main__':
             os.makedirs(output_dir)
         logger_kwargs = dict(output_dir=output_dir, exp_name=exp_name)
         sac(env_fn, ac_kwargs={}, seed=0, steps_per_epoch=100, epochs=200, replay_size=1000000, gamma=0.99, polyak=0.995,
-            lr=1e-1, alpha_init=0.001, batch_size=100, start_steps=1000, update_after=1000, update_every=100, num_test_episodes=2,
+            lr=0.005, alpha_init=0.001, batch_size=100, start_steps=1000, update_after=1000, update_every=100, num_test_episodes=2,
             max_ep_len=np.inf, logger_kwargs=logger_kwargs, save_freq=1, initial_actions="random", save_buffer=True, sample_mode = 1, automatic_entropy_tuning=True)
     else:
         output_dir='/home/mahdi/ETHZ/codes/spinningup/spinup/examples/pytorch/logs/'+exp_name
