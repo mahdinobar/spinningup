d6447b5803b40bc34897440c1a1430b1d474fe61
mahdinobar <nobar.mahdi@gmail.com>
2024-05-02 14:13:56 +0200

    Fepv0_14_TargetEntropy01times_lp65_50percentAction

--- spinup/utils/test_policy.py
+++ spinup/utils/test_policy.py
@@ -9,6 +9,7 @@ from spinup.utils.logx import restore_tf_graph
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def load_policy_and_env(fpath, itr='last', deterministic=False):
     """
     Load a policy from save, whether it's TF or PyTorch, along with RL env.
@@ -28,25 +29,25 @@ def load_policy_and_env(fpath, itr='last', deterministic=False):
         backend = 'pytorch'
 
     # handle which epoch to load from
-    if itr=='last':
+    if itr == 'last':
         # check filenames for epoch (AKA iteration) numbers, find maximum value
 
         if backend == 'tf1':
-            saves = [int(x[8:]) for x in os.listdir(fpath) if 'tf1_save' in x and len(x)>8]
+            saves = [int(x[8:]) for x in os.listdir(fpath) if 'tf1_save' in x and len(x) > 8]
 
         elif backend == 'pytorch':
             pytsave_path = osp.join(fpath, 'pyt_save')
             # Each file in this folder has naming convention 'modelXX.pt', where
             # 'XX' is either an integer or empty string. Empty string case
             # corresponds to len(x)==8, hence that case is excluded.
-            saves = [int(x.split('.')[0][5:]) for x in os.listdir(pytsave_path) if len(x)>8 and 'model' in x]
+            saves = [int(x.split('.')[0][5:]) for x in os.listdir(pytsave_path) if len(x) > 8 and 'model' in x]
 
-        itr = '%d'%max(saves) if len(saves) > 0 else ''
+        itr = '%d' % max(saves) if len(saves) > 0 else ''
 
     else:
         assert isinstance(itr, int), \
             "Bad value provided for itr (needs to be int or 'last')."
-        itr = '%d'%itr
+        itr = '%d' % itr
 
     # load the get_action function
     if backend == 'tf1':
@@ -57,7 +58,7 @@ def load_policy_and_env(fpath, itr='last', deterministic=False):
     # try to load environment from save
     # (sometimes this will fail because the environment could not be pickled)
     try:
-        state = joblib.load(osp.join(fpath, 'vars'+itr+'.pkl'))
+        state = joblib.load(osp.join(fpath, 'vars' + itr + '.pkl'))
         env = state['env']
     except:
         env = None
@@ -68,8 +69,8 @@ def load_policy_and_env(fpath, itr='last', deterministic=False):
 def load_tf_policy(fpath, itr, deterministic=False):
     """ Load a tensorflow policy saved with Spinning Up Logger."""
 
-    fname = osp.join(fpath, 'tf1_save'+itr)
-    print('\n\nLoading from %s.\n\n'%fname)
+    fname = osp.join(fpath, 'tf1_save' + itr)
+    print('\n\nLoading from %s.\n\n' % fname)
 
     # load the things!
     sess = tf.Session()
@@ -85,16 +86,16 @@ def load_tf_policy(fpath, itr, deterministic=False):
         action_op = model['pi']
 
     # make function for producing an action given a single state
-    get_action = lambda x : sess.run(action_op, feed_dict={model['x']: x[None,:]})[0]
+    get_action = lambda x: sess.run(action_op, feed_dict={model['x']: x[None, :]})[0]
 
     return get_action
 
 
 def load_pytorch_policy(fpath, itr, deterministic=False):
     """ Load a pytorch policy saved with Spinning Up Logger."""
-    
-    fname = osp.join(fpath, 'pyt_save', 'model'+itr+'.pt')
-    print('\n\nLoading from %s.\n\n'%fname)
+
+    fname = osp.join(fpath, 'pyt_save', 'model' + itr + '.pt')
+    print('\n\nLoading from %s.\n\n' % fname)
 
     model = torch.load(fname)
 
@@ -102,20 +103,24 @@ def load_pytorch_policy(fpath, itr, deterministic=False):
     def get_action(x):
         with torch.no_grad():
             x = torch.as_tensor(x, dtype=torch.float32)
-            action = model.act(x)
+            # action = model.act(x)
+            action = model.act(x, deterministic=True)
         return action
+
+    # ac.act(torch.as_tensor(o, dtype=torch.float32),
+    #        deterministic)
 
     return get_action
 
 
 def run_policy(env, get_action, max_ep_len=None, num_episodes=100, render=True, output_dir=""):
-
     assert env is not None, \
         "Environment not found!\n\n It looks like the environment wasn't saved, " + \
         "and we can't run the agent in it. :( \n\n Check out the readthedocs " + \
         "page on Experiment Outputs for how to handle this situation."
     logger = EpochLogger()
-    o, r, d, ep_ret, ep_len, n = env.reset(), 0, False, 0, 0, 0
+    o, d, ep_ret, ep_len, n, r = env.reset(), False, 0, 0, 0, 0
+
     while n < num_episodes:
         a = get_action(o)
         o, r, d, _ = env.step(a)
@@ -123,48 +128,49 @@ def run_policy(env, get_action, max_ep_len=None, num_episodes=100, render=True, 
         ep_len += 1
 
         if d or (ep_len == max_ep_len):
-            if render:
+            if "Fep" in output_dir:
                 env.render(output_dir)
-                # time.sleep(1e-3)
+                logger.store(EpRet=ep_ret, EpLen=ep_len)
             if "Tworr" in output_dir:
                 # plotting for planar (modify for others)
                 # np.save(
                 #     "/home/mahdi/ETHZ/codes/spinningup/spinup/examples/pytorch/logs/no_SAC_0_12/no_SAC_plot_data_buffer.npy",
                 #     env.plot_data_buffer)
-                no_SAC_plot_data_buffer=np.load(
+                no_SAC_plot_data_buffer = np.load(
                     "/home/mahdi/ETHZ/codes/spinningup/spinup/examples/pytorch/logs/no_SAC_0_12/no_SAC_plot_data_buffer.npy")
                 plt.figure(1)
                 plt.rcParams["font.family"] = "Times New Roman"
-                plt.plot(env.plot_data_buffer[:,2], env.plot_data_buffer[:,3], 'r--', label='rd: desired traj')
-                plt.plot(env.plot_data_buffer[:, 0], env.plot_data_buffer[:, 1], 'b', label='r_hat: FK estimated traj with Hybrid controller')
-                plt.plot(no_SAC_plot_data_buffer[:, 0], no_SAC_plot_data_buffer[:, 1], 'k', label='no_SAC_r_hat: FK estimated traj with PID only controller')
+                plt.plot(env.plot_data_buffer[:, 2], env.plot_data_buffer[:, 3], 'r--', label='rd: desired traj')
+                plt.plot(env.plot_data_buffer[:, 0], env.plot_data_buffer[:, 1], 'b',
+                         label='r_hat: FK estimated traj with Hybrid controller')
+                plt.plot(no_SAC_plot_data_buffer[:, 0], no_SAC_plot_data_buffer[:, 1], 'k',
+                         label='no_SAC_r_hat: FK estimated traj with PID only controller')
                 # plt.rcParams["font.family"] = "sans-serif"
                 # plt.rcParams["font.serif"] = "Times New Roman"
                 plt.xlabel("x")
                 plt.ylabel("y")
                 plt.legend()
-                plt.savefig(output_dir+"/position.pdf",format="pdf", bbox_inches='tight')
+                plt.savefig(output_dir + "/position.pdf", format="pdf", bbox_inches='tight')
                 plt.show()
                 plt.figure(2)
                 plt.rcParams["font.family"] = "Times New Roman"
-                plt.plot(env.plot_data_buffer[:,6], env.plot_data_buffer[:,7], 'r--', label='vd: desired traj', marker=".", markersize=30)
+                plt.plot(env.plot_data_buffer[:, 6], env.plot_data_buffer[:, 7], 'r--', label='vd: desired traj',
+                         marker=".", markersize=30)
                 plt.plot(env.plot_data_buffer[:, 4], env.plot_data_buffer[:, 5], 'b', label='v_hat: FK estimated traj')
-                plt.plot(no_SAC_plot_data_buffer[:, 4], no_SAC_plot_data_buffer[:, 5], 'k', label='no_SAC_v_hat: FK estimated traj with PID only controller')
+                plt.plot(no_SAC_plot_data_buffer[:, 4], no_SAC_plot_data_buffer[:, 5], 'k',
+                         label='no_SAC_v_hat: FK estimated traj with PID only controller')
 
                 # plt.rcParams["font.family"] = "sans-serif"
                 # plt.rcParams["font.serif"] = "Times New Roman"
                 plt.xlabel("vx")
                 plt.ylabel("vy")
                 plt.legend()
-                plt.savefig(output_dir+"/velocity.pdf",format="pdf", bbox_inches='tight')
+                plt.savefig(output_dir + "/velocity.pdf", format="pdf", bbox_inches='tight')
                 plt.show()
-
-
-            logger.store(EpRet=ep_ret, EpLen=ep_len)
-            print('Episode %d \t EpRet %.3f \t EpLen %d'%(n, ep_ret, ep_len))
-            o, r, d, ep_ret, ep_len = env.reset(), 0, False, 0, 0
+                logger.store(EpRet=ep_ret, EpLen=ep_len)
             n += 1
-
+        print('Episode %d \t EpRet %.3f \t EpLen %d' % (n, ep_ret, ep_len))
+        o, r, d, ep_ret, ep_len = env.reset(), 0, False, 0, 0
     logger.log_tabular('EpRet', with_min_and_max=True)
     logger.log_tabular('EpLen', average_only=True)
     logger.dump_tabular()
@@ -172,15 +178,17 @@ def run_policy(env, get_action, max_ep_len=None, num_episodes=100, render=True, 
 
 if __name__ == '__main__':
     import argparse
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('fpath', type=str, default="/home/mahdi/ETHZ/codes/spinningup/spinup/examples/pytorch/logs/Tworrv0_10")
+    parser.add_argument('fpath', type=str,
+                        default="/home/mahdi/ETHZ/codes/spinningup/spinup/examples/pytorch/logs/Tworrv0_10")
     parser.add_argument('--len', '-l', type=int, default=0)
     parser.add_argument('--episodes', '-n', type=int, default=10)
     parser.add_argument('--norender', '-nr', action='store_true')
     parser.add_argument('--itr', '-i', type=int, default=-1)
     parser.add_argument('--deterministic', '-d', action='store_true')
     args = parser.parse_args()
-    env, get_action = load_policy_and_env(args.fpath, 
-                                          args.itr if args.itr >=0 else 'last',
+    env, get_action = load_policy_and_env(args.fpath,
+                                          args.itr if args.itr >= 0 else 'last',
                                           args.deterministic)
-    run_policy(env, get_action, args.len, args.episodes, not(args.norender))
+    run_policy(env, get_action, args.len, args.episodes, not (args.norender))
