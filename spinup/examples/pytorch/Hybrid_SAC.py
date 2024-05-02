48c8389166b294e568259db56da45f5ebf8d0a83
mahdinobar <nobar.mahdi@gmail.com>
2024-05-02 10:36:01 +0200

    Fepv0_14_TargetEntropy1times_lp65_initactionZero

--- spinup/examples/pytorch/Hybrid_SAC.py
+++ spinup/examples/pytorch/Hybrid_SAC.py
@@ -9,7 +9,7 @@ import os
 
 TRAIN=1
 env_fn = lambda: gym.make('Fep-v0')
-exp_name = "Fepv0_14_TargetEntropy01times_lp65"
+exp_name = "Fepv0_14_TargetEntropy1times_lp65_initactionZero"
 if __name__ == '__main__':
     if TRAIN:
         # train
@@ -19,7 +19,7 @@ if __name__ == '__main__':
         logger_kwargs = dict(output_dir=output_dir, exp_name=exp_name)
         sac(env_fn, ac_kwargs={}, seed=0, steps_per_epoch=100, epochs=300, replay_size=1000000, gamma=0.99, polyak=0.995,
             lr=0.01, alpha_init=0.001, batch_size=100, start_steps=10000, update_after=10000, update_every=100, num_test_episodes=2,
-            max_ep_len=np.inf, logger_kwargs=logger_kwargs, save_freq=1, initial_actions="random", save_buffer=True, sample_mode = 1, automatic_entropy_tuning=True)
+            max_ep_len=np.inf, logger_kwargs=logger_kwargs, save_freq=1, initial_actions="zero", save_buffer=True, sample_mode = 1, automatic_entropy_tuning=True)
     else:
         output_dir='/home/mahdi/ETHZ/codes/spinningup/spinup/examples/pytorch/logs/'+exp_name
         env_loaded, get_action = load_policy_and_env(output_dir)
