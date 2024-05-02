498ce2807a6d8e40f3e7ab9c4c7a668509b8b4e4
mahdinobar <nobar.mahdi@gmail.com>
2024-05-02 15:26:49 +0200

    Fepv0_14_TargetEntropy01times_lp65_5percentAction

--- spinup/examples/pytorch/Hybrid_SAC.py
+++ spinup/examples/pytorch/Hybrid_SAC.py
@@ -9,7 +9,7 @@ import os
 
 TRAIN=1
 env_fn = lambda: gym.make('Fep-v0')
-exp_name = "Fepv0_14_TargetEntropy01times_lp65_50percentAction"
+exp_name = "Fepv0_14_TargetEntropy01times_lp65_5percentAction"
 if __name__ == '__main__':
     if TRAIN:
         # train
