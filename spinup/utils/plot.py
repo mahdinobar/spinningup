2167583c938e10242a347425403ff377d5ca51f4
mahdinobar <nobar.mahdi@gmail.com>
2024-05-03 18:35:41 +0200

    Fepv0_15_lr0002_5000epochs_lp100

--- spinup/utils/plot.py
+++ spinup/utils/plot.py
@@ -161,7 +161,7 @@ def make_plots(all_logdirs, legend=None, xaxis=None, values=None, count=False,
         plt.figure()
         plot_data(data, xaxis=xaxis, value=value, condition=condition, smooth=smooth, estimator=estimator)
     # plt.hlines(44.47124, 0, 10e4, 'k', linestyles="dashed", label='PID only')
-    plt.hlines( 64.6, 0, 10e4, 'k', linestyles="dashed", label='PID only')
+    plt.hlines( 64.6, 0, 10e5, 'k', linestyles="dashed", label='PID only')
     plt.savefig(all_logdirs[-1]+"learning_curve", format="pdf", bbox_inches='tight')
     plt.show()
 
