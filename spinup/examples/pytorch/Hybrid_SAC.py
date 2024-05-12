2d1d7b4e07002de74179267cc2b9f71ec3f354b4
mahdinobar <nobar.mahdi@gmail.com>
2024-05-12 21:55:25 +0200

    dev

--- spinup/examples/pytorch/Hybrid_SAC.py
+++ spinup/examples/pytorch/Hybrid_SAC.py
@@ -7,9 +7,9 @@ import spinup.algos.pytorch.sac.core as core
 from spinup.utils.test_policy import load_policy_and_env, run_policy
 import os
 
-TRAIN=0
+TRAIN=1
 env_fn = lambda: gym.make('Fep-v0')
-exp_name = "Fepv0_15_lr0001_5000epochs_lp130_separated_pose_errors_qinitnoisy"
+exp_name = "Fepv0_16"
 if __name__ == '__main__':
     if TRAIN:
         # train
