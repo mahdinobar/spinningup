e55985da921169766f6e1e62bbbd509fe0296e34
mahdinobar <nobar.mahdi@gmail.com>
2024-05-15 15:32:26 +0200

    Fep3v0_17_biased_1, #2

--- spinup/utils/test_policy.py
+++ spinup/utils/test_policy.py
@@ -171,7 +171,6 @@ def run_policy(env, get_action, max_ep_len=None, num_episodes=100, render=True, 
             logger.store(EpRet=ep_ret, EpLen=ep_len)
             print('Episode %d \t EpRet %.3f \t EpLen %d' % (n, ep_ret, ep_len))
             o, r, d, ep_ret, ep_len = env.reset(), 0, False, 0, 0
-            n += 1
 
     logger.log_tabular('EpRet', with_min_and_max=True)
     logger.log_tabular('EpLen', average_only=True)
