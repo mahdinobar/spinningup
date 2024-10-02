dde98ecdb13f910d4dc7218d87dedb4ca720e605
mahdinobar <nobar.mahdi@gmail.com>
2024-10-02 11:21:12 +0200

    draft_HW_4_NOSAC

--- spinup/backup_gym/gym/envs/classic_control/fep.py
+++ spinup/backup_gym/gym/envs/classic_control/fep.py
@@ -68,15 +68,15 @@ Robotic Manipulation" by Murry et al.
         np.random.seed(seed)
         self.seed(seed=seed)
         # TODO: reward params
-        self.lp = 800
+        self.lp = 400
         self.lv = 10
         self.lddqc = 1
         self.reward_eta_p = 1
         self.reward_eta_v = 0
         self.reward_eta_ddqc = 0
         # TODO: User defined linear position gain
-        self.K_p = 10
-        self.K_i = 10
+        self.K_p = 1
+        self.K_i = 1
         self.K_d = 0
         self.korque_noise_max = 0.  # TODO
         self.viewer = None
@@ -374,7 +374,7 @@ Robotic Manipulation" by Murry et al.
         dqc_t, self.e = self.q_command(r_ee=r_hat_t, v_ee=v_hat_t, Jpinv=Jpinv_t, rd=rd_t, vd=vd_t, e=self.e,
                                        dt=dt)
         # ATTENTION: here apply SAC action
-        dqc_t = dqc_t + a
+        dqc_t = dqc_t #+ a
         # TODO check
         # command joint speeds (only 6 joints)
         pb.setJointMotorControlArray(
@@ -500,8 +500,8 @@ Robotic Manipulation" by Murry et al.
                        rd_t_error[1],
                        rd_t_error[2]]
         self.plot_data_buffer = np.vstack((self.plot_data_buffer, plot_data_t))
-        # plot_data_buffer_no_SAC=self.plot_data_buffer
-        # np.save("/home/mahdi/ETHZ/codes/spinningup/spinup/examples/pytorch/logs/draft_HW_3_NOSAC/plot_data_buffer_no_SAC.npy",plot_data_buffer_no_SAC)
+        plot_data_buffer_no_SAC=self.plot_data_buffer
+        np.save("/home/mahdi/ETHZ/codes/spinningup/spinup/examples/pytorch/logs/draft_HW_4_NOSAC/plot_data_buffer_no_SAC.npy",plot_data_buffer_no_SAC)
         # given action it returns 4-tuple (observation, reward, done, info)
         return (self._get_ob(), reward_t, terminal, {})
 
