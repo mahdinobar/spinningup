3a10534317935a1f6a2ac69cffa62e2cec77eee4
mahdinobar <nobar.mahdi@gmail.com>
2025-07-29 21:21:27 +0200

    HW_314, #2 corrected

--- spinup/examples/pytorch/Hybrid_SAC.py
+++ spinup/examples/pytorch/Hybrid_SAC.py
@@ -9,11 +9,11 @@ from spinup.algos.pytorch.sac.sac import load_checkpoint
 from spinup.utils.test_policy import load_policy_and_env, run_policy
 import os
 
-TRAIN=0
+TRAIN=1
 env_fn = lambda: gym.make('Fep-v0')
 exp_name = "Fep_HW_314"
 exp_name_checkpoint = "Fep_HW_309"
-Euler_server=False
+Euler_server=True
 if __name__ == '__main__':
     if Euler_server==True:
         output_dir='/cluster/home/mnobar/code/spinningup/spinup/examples/pytorch/logs/'+exp_name
